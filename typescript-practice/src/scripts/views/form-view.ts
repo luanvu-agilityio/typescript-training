80ac5040a77ea3ec1aee151d812cb98e8d6e3fed
Luan Vu <luan.vu@asnet.com.vn>
2025-02-26 15:26:41 +0700

    update date convert method

--- typescript-practice/src/scripts/views/form-view.ts
+++ typescript-practice/src/scripts/views/form-view.ts
@@ -102,7 +102,11 @@ export class StudentFormView {
     const date = parseDate(dateString);
     // Make sure date is valid before formatting
     if (isNaN(date.getTime())) return '';
-    return date.toISOString().split('T')[0];
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 
   /**
